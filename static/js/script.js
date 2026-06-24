aa9160de0ad4fb4838f42a03d5da9b5629c245a7
Uppalapati Asmitha Sai <asmithasai890@gmail.com>
2026-06-24 23:58:05 +0530

    Add confirmation and alert functions for cart actions

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+function confirmDelete(){
+
+return confirm("Delete this item?");
+
+}
+
+function cartMessage(){
+
+alert("Item Added To Cart");
+
+}
+
+function checkout(){
+
+alert("Order Placed Successfully");
+
+}
