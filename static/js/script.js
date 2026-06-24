5d20b683c9d1251a86807f741877697681b57cff
Uppalapati Asmitha Sai <asmithasai890@gmail.com>
2026-06-24 23:51:40 +0530

    Add JavaScript functions for delete and cart actions

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+function confirmDelete(){
+    return confirm("Are you sure you want to delete this product?");
+}
+
+function addCart(){
+    alert("Product added to cart.");
+}
+
+function checkoutSuccess(){
+    alert("Order placed successfully.");
+}
